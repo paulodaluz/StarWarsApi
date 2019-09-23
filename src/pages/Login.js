09d8470c271b77c4ad5efc290df277dec6b3f59f
Paulo da Luz <paulo.luz@compasso.com.br>
2019-09-23 17:23:23 -0300

    Função de login implementada

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export default class Login extends Component {
     render() {
         return (
             <div style={{ 'padding': '200px 200px 100px 0' }}>
-                <p>Login aqui</p>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Endereço de email</Form.Label>
+                        <Form.Control type="email" placeholder="Digite seu email" />
+                            <Form.Text className="text-muted">
+                                Nunca compartilharemos seu email com mais ninguém.
+                            </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Senha</Form.Label>
+                        <Form.Control type="password" placeholder="Digite sua senha" />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">Logar</Button>
+                    
+                </Form>
+
             </div>
         )
     }
