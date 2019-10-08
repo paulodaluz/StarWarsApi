3def0042ed8ed752f8327ad54102aeceb8f04cd3
Paulo da Luz <paulo.luz@compasso.com.br>
2019-10-08 15:41:24 -0300

    Implementada captação de email e senha no loading

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -7,23 +7,46 @@ import '../../App.css';
 import './Login.css';
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            senha: ''            
+        };
+    }
+    logar() {
+        var email = this.state.email
+        var senha = this.state.senha
+        console.log(email, senha)
+    }
+
     render() {
         return (
             <div className="padding-login">
                 <div className="centralizar">
                     <h1 className="centraliza-titulo">Login</h1>
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Endereço de email</Form.Label>
-                        <Form.Control type="email" placeholder="Digite seu email" />
-                    </Form.Group>
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Endereço de email</Form.Label>
+                            <Form.Control type="email" placeholder="Digite seu email" 
+                                onInput={(e) => 
+                                    this.setState({ email: e.target.value })}
+                            />
+                        </Form.Group>
 
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Senha</Form.Label>
-                        <Form.Control type="current-password" placeholder="Digite sua senha" />
-                    </Form.Group>
-                        <Button variant="dark" type="submit" className="button-logar">Logar</Button>
-                </Form>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Senha</Form.Label>
+                                <Form.Control type="password" placeholder="Digite sua senha"
+                                    onInput={(e) => 
+                                        this.setState({ senha: e.target.value })}
+                                />
+                        </Form.Group>
+
+                            <Button variant="dark" className="button-logar" 
+                            onClick={() => this.logar()} 
+                            >Logar</Button>
+                            
+                    </Form>
                 </div>
             </div>
         )
