f02fd468b630884c33e2795c9fd05e2794c42da9
Paulo da Luz <paulo.luz@compasso.com.br>
2019-10-08 09:25:06 -0300

    Ajustes pontuais

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -19,36 +19,27 @@ export default class Home extends Component {
                                         <h2>Paulo Ricardo da Luz Júnior</h2>
                                         <h3>1116505@imed.edu.br</h3>
                                         <p>RA: 1116505</p>
-                                        {/* <a className="banner_btn" href="/">Enviar um Email</a> */}
                                         <Link to="/"><Button className="banner_btn" variant="light">Enviar um Email</Button></Link>
                                     </div>
                                 </div>
                                 <div className="col-lg-4">
                                     <div style={{ 'text-transform': 'uppercase', 'font-family': "Heebo, sans-serif" }} className="home_right_box">
                                         <div className="home_item">
-                                            {/* <i className="flaticon-sofa"></i> */}
                                             <Link to="/pessoas" className="navbar-brand logo_h" style={{ color: 'white' }}>Pessoas</Link>
                                         </div>
                                         <div className="home_item">
-                                            {/* <i className="flaticon-bed"></i> */}
                                             <Link to="/filmes" className="navbar-brand logo_h" style={{ color: 'white' }}>Filmes</Link>
                                         </div>
                                         <div className="home_item">
-                                            {/* <i className="flaticon-computer"></i> */}
                                             <Link to="/planetas" className="navbar-brand logo_h" style={{ color: 'white' }}>Planetas</Link>
                                         </div>
                                         <div className="home_item">
-                                            {/* <i className="flaticon-mirror"></i> */}
                                             <Link to="/species" className="navbar-brand logo_h" style={{ color: 'white' }}>Espécies</Link>
                                         </div>
                                         <div className="home_item">
-                                            <div >
-                                            {/* <i className="flaticon-closet"></i> */}
                                                 <Link to="/starships" className="navbar-brand logo_h" style={{ color: 'white' }}>Naves</Link>
-                                            </div>
                                         </div>
                                         <div className="home_item">
-                                            {/* <i className="flaticon-kitchen"></i> */}
                                             <Link to="/login" className="navbar-brand logo_h" style={{ color: 'white' }}>Login</Link>
                                         </div>
                                     </div>
